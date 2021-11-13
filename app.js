37bea5cb1eb63063903f9fd814c60a1866cb1794
Jessy93 <adiatu.ibrahim@gmail.com>
2021-11-13 16:08:00 +0400

    start to auto-deplay with github on main

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ import lottieWeb from 'https://cdn.skypack.dev/lottie-web';
 
 let container = document.getElementById('container');
 let svg = document.getElementById('svg');
-
 let state = 'play';
 
 let animation = lottieWeb.loadAnimation({
@@ -13,6 +12,7 @@ let animation = lottieWeb.loadAnimation({
   loop: false,
   autoplay: false,
   name: "Demo Animation",
+  // animationData: text_demo_data,
 });
 
 animation.goToAndStop(0, true);
@@ -30,6 +30,8 @@ container.addEventListener('click', () => {
   }
 });
 
+// Text Random
 const textArray = ['1a','2b', '3c', '4e', '5f'];
 let phrase = textArray[Math.floor(Math.random()*textArray.length)];
 console.log(phrase);
+text_demo_data.layers[0].t.d.k[0].s.t = phrase;
