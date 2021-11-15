1765df84948437d1b34139c59729560bcd369bff
Jessy93 <adiatu.ibrahim@gmail.com>
2021-11-15 12:25:13 +0400

    reseting 4

---
+++ public/assets/script/effect.js
@@ -0,0 +1,21 @@
+
+let parent = document.getElementById('container-sequins');
+
+function makeSequins() {
+  for (let index = 0; index < 500; index++) {
+    let sequin = document.createElement("div"); 
+    sequin.innerHTML = `
+      <div id="sequin" 
+        style="top: ${(Math.random() * 100) + '%'}; 
+        left: ${(Math.random() * 100) + '%'};
+        right: ${(Math.random() * 100) + '%'};
+        bottom: ${(Math.random() * 100) + '%'};
+        animation-delay: ${(Math.random() * 100) + '%'}">
+      </div>`;
+
+    console.log(sequin);
+    parent.append(sequin);
+  }
+}
+
+// makeSequins();
