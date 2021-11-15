238ecd1b7072c45d81d3e5630bf6ac6640244eab
Jessy93 <adiatu.ibrahim@gmail.com>
2021-11-15 10:48:03 +0400

    checking

--- assets-3/script/effect.js
+++ assets-3/script/effect.js
@@ -3,11 +3,13 @@ let parent = document.getElementById('container-sequins');
 
 
 for (let index = 0; index < 500; index++) {
-  let sequin = document.createElement("div sequin"); 
+  let sequin = document.createElement("div"); 
   sequin.innerHTML = `
-    <div  
+    <div id="sequin" 
       style="top: ${(Math.random() * 100) + '%'}; 
-      left: ${(Math.random() * 100) + '%'}
+      left: ${(Math.random() * 100) + '%'};
+      right: ${(Math.random() * 100) + '%'};
+      bottom: ${(Math.random() * 100) + '%'};
       animation-delay: ${(Math.random() * 100) + '%'}">
     </div>`;
 
